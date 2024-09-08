ddc9fee0d800a765e2f24dbe52f13b7717439359
AayushSharma-1 <aayush9119@gmail.com>
2024-09-08 23:30:21 +0530

    made_changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,31 +10,31 @@ with open('category1.txt', 'r') as f:
     CategoryOptions = [tag.text for tag in soup2.find_all('option')]
 
 def get_details(data):
-    title = data.find('h3', class_='heading_4_5 profile').text.strip()
+    title = data.find('h3', class_='job-internship-name').text.strip()
     company_name = data.find('div', class_='company_and_premium').find('p').text.strip()
-    location = [loc.text for loc in data.find('div', id='location_names').find_all('a')]
-    start_date = data.find('span', class_='start_immediately_desktop').text if data.find('span', class_='start_immediately_desktop') else "Not Available"
-    duration = data.find('div', class_='item_body').text.strip()
-    stipend = re.findall('₹ \d+.\d+',str(data.find('div', attrs = {'class' : 'other_detail_item stipend_container'})))
-    status = data.find_all('i', class_='ic-16-reschedule')[0].next_sibling.strip()
-    links = 'https://internshala.com' + data.find('a', class_='btn btn-secondary view_detail_button_outline')['href']
+    location = [loc.text for loc in data.find('div', class_='row-1-item locations').find_all('a')]
+    # start_date = data.find('span', class_='start_immediately_desktop').text if data.find('span', class_='start_immediately_desktop') else "Not Available"
+    duration = re.findall(r'<i class="ic-16-calendar"></i>\s*<span>([^<]+)</span>', str(dee[0].find_all('div', class_='row-1-item')))[0].strip()
+    stipend = re.findall('₹ \d+.\d+.[^/]+',str(dee[0].find('span', attrs = {'class' : 'stipend'})))[0].strip()
+    # status = data.find_all('i', class_='ic-16-reschedule')[0].next_sibling.strip()
+    # links = 'https://internshala.com' + data.find('a', class_='btn btn-secondary view_detail_button_outline')['href']
     
     return {
         "Title": title,
         "Company Name": company_name,
         "Location": location,
-        "Start Date": start_date,
+        # "Start Date": start_date,
         "Duration": duration,
         "Stipend": stipend,
-        "Status": status,
-        "Links": links
+        # "Status": status,
+        # "Links": links
     }
 
 st.write(""" # Internshala Scraper""")
 selections = st.sidebar.multiselect('Pick The Domains', CategoryOptions)
 link = 'https://internshala.com/internships/'
 for i in range(len(selections)):    
-    selections[i] = ','+ selections[i].replace(" ", '-').lower()
+    selections[i] = ','+selections[i].replace(" ", '-').lower()
     link+=selections[i]
 
 print(link)
@@ -50,12 +50,15 @@ if response.status_code == 200:
         res_link = f"{link + '-internship'}/page-{j}"
         print(res_link)
         res = requests.get(res_link)
+        print(res.status_code)
         res_soup = BeautifulSoup(res.content, 'html.parser')
-        dee = res_soup.find_all('div', class_='container-fluid individual_internship visibilityTrackerItem')
-        
+        dee = res_soup.find_all('div', attrs={'class' :'container-fluid individual_internship view_detail_button visibilityTrackerItem'})
+        # print(dee)
         for item in dee:
             dict_of_data.append(get_details(item))
 
     df = pd.DataFrame(dict_of_data)
     df.index = pd.RangeIndex(start = 1, stop = len(df) + 1, name = 'Index')
     st.dataframe(df, use_container_width=True)
+else: 
+    print("Status code not 200")
